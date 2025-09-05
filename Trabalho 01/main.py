db5f7c07702700acff8bbbf1583e1f6978772694
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-09-05 16:37:40 -0300

    feat: adicionando pipeline de teste

--- Trabalho 01/main.py
+++ Trabalho 01/main.py
@@ -1,8 +1,19 @@
 from utils import generate_data
 
+def linear_array_test(data):
+    pass
+
+def tree_test(data):
+    pass
+
+def hash_test(data):
+    pass
+
 def main():    
     data = generate_data(50_000)
-    print(data)
+    linear_array_test(data)
+    tree_test(data)
+    hash_test(data)
 
 if __name__ == "__main__":
     main()
