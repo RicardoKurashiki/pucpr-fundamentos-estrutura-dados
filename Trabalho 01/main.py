6381d798119f607ce99ce4acbe196a2a0b8ba7b4
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-09-13 10:06:29 -0300

    chore: limpando código

--- Trabalho 01/main.py
+++ Trabalho 01/main.py
@@ -4,57 +4,11 @@ import psutil as p
 import random as rd
 import os
 import csv
-from tqdm import tqdm
 import tracemalloc
 
 from hashtable import HashTable
 from avltree import AVLTree
 from unbaltree import UnBalTree
-
-def compute_algorithms(data, func, name, iteration, size):
-
-    # Objeto Process que representa o nosso script atual
-    process = p.Process(os.getpid())
-    # Mede o tempo de CPU inicial
-    cpu_before = process.cpu_times()
-
-    p.cpu_percent()
-    tracemalloc.start()
-
-    # A função de teste pode retornar um dicionário de métricas específicas
-    specific_metrics = func(data)
-
-    # Mede o tempo de CPU final
-    cpu_after = process.cpu_times()
-
-    #memory_usage = tracemalloc.get_tracemalloc_memory()
-    current_mem, peak_mem = tracemalloc.get_traced_memory()
-    tracemalloc.stop()
-
-    # Calcula a diferença de tempo de CPU
-    cpu_time_user = cpu_after.user - cpu_before.user
-    cpu_time_system = cpu_after.system - cpu_before.system
-    total_cpu_time = cpu_time_user + cpu_time_system
-    # Usaremos o valor de PICO (peak_mem) para o nosso relatório.
-    memory_usage = peak_mem
-
-    p_cpu_usage = p.cpu_percent()
-
-    os.makedirs("./outputs/", exist_ok=True)
-
-    with open(f"./outputs/{name}_{size}.txt", "a") as f:
-        f.writelines(
-            "--------------------------------\n\n"
-            f"| Size: {size}\n"
-            f"| Iteration: {iteration}\n"
-            f"| Memory Usage: {memory_usage:9d} bytes | CPU Usage: {p_cpu_usage} % \n"
-            f"| CPU Time (user+system): {total_cpu_time:.4f} s\n"
-        )
-        # Loop genérico para salvar as eventuais métricas retornadas
-        if specific_metrics is not None:
-            for metric_name, metric_value in specific_metrics.items():
-                f.writelines(f"| {metric_name}: {metric_value}\n")
-        f.writelines("\n")
 
 def compute_and_log_metrics(metrics, name, iteration, size):
     """
@@ -272,20 +226,18 @@ def hash_test(data):
     hash_table.search(data[len(data) // 2][0])
 
 def main():
-    sizes = [50_000, 100_000, 500_000, 1_000_000]
     rd.seed(42)
+    sizes = [50_000, 100_000, 500_000, 1_000_000]
     iterations = 5
-    for size in tqdm(sizes):
+    for size in sizes:
         """ Cada conjunto de dados de tamanho N é gerado uma única vez pois fixamos a semente aleatória para garantir a
          reprodutibilidade do experimento. Caso se deseje uma ideia da melhor performance para o "caso médio geral", poderia
          ser removida a inicialização da semente aleatória, e a geração do conjunto de dado seria feita no loop interno
          """
         data = generate_data(size)
         for i in range(iterations):
-            # data = generate_data(size) # para o caso de não ser fixada a semente aleatória
             array_metrics = linear_array_test(data)
             compute_and_log_metrics(array_metrics, "linear_array", i, size)
-            #compute_algorithms(data, hash_test, "hash_table", i, size)
 
             unbaltree_metrics = test_unbaltree_lifecycle(data)
             compute_and_log_metrics(unbaltree_metrics, "regular_tree", i, size)
