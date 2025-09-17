c826f27261f640f0113c7d6533fb33bba8f5c42a
uendelrocha <uendelrocha@gmail.com>
2025-09-17 19:29:46 -0300

    Ajustes na função de registros aleatórios

--- Trabalho 01/utils.py
+++ Trabalho 01/utils.py
@@ -3,7 +3,6 @@ import random as rd
 
 import pandas as pd
 from hashtables.tests import nome_aleatorio
-import pickle
 
 def generate_data(n):
     """
@@ -28,9 +27,11 @@ def gen_hashtable_samples(num_elements,  **kwargs):
     
     # Valores default para funcionamento da amostragem
     config = {
-        'filename': r'.\outputs\data.gz',
+        'filename': r'.\outputs\data.pkl.gz',
         'p_hat_dense': 0.5,
         'seed': 42,
+        'start_range': 100_000_000,
+        'final_range': 999_999_999
         }
 
     # Atualiza configuração conforme parâmetros
@@ -38,6 +39,8 @@ def gen_hashtable_samples(num_elements,  **kwargs):
     filename = config['filename']
     p_hat_dense = config['p_hat_dense']
     seed = config['seed']
+    start_range = config['start_range']
+    final_range = config['final_range']
     
     rd.seed(seed)
 
@@ -49,25 +52,26 @@ def gen_hashtable_samples(num_elements,  **kwargs):
     p_dense = p_hat_dense # Proporção de elementos densos da lista: quanto menor, mais denso
     dense_range_size = int(num_elements * p_dense)
     
-    # Define o intervalo para lista esparsa (com menos elementos por intervalo)
-    sparse_range_start = dense_range_size + 1
-    sparse_range_end = int((num_elements - dense_range_size) ** 2)
-    
     # Calcula o número de chaves para cada intervalo
     num_dense_keys = int(dense_range_size * (1 - p_dense))
     num_sparse_keys = num_elements - num_dense_keys
+
+    # Define o intervalo para lista esparsa (com menos elementos por intervalo)
+    sparse_range_start = start_range + dense_range_size + 1
+    sparse_range_end = int((num_elements - dense_range_size) ** 2) if final_range <= (sparse_range_start + num_sparse_keys) else final_range
+    
     
     # Gera as chaves aleatórias
-    dense_keys = rd.sample(range(1, dense_range_size + 1), num_dense_keys)
+    dense_keys = rd.sample(range(start_range, start_range + dense_range_size), num_dense_keys)
     sparse_keys = rd.sample(range(sparse_range_start, sparse_range_end + 1), num_sparse_keys)
     
     all_keys = dense_keys + sparse_keys
     rd.shuffle(all_keys)
 
     data = []
-    nomes_aleatorios = nome_aleatorio.gerar_nomes_aleatorios(seed, num_elements)
+    nomes_aleatorios = nome_aleatorio.gerar_nomes_aleatorios(num_elements, seed)
     for i, k in enumerate(all_keys):
-        id = k if k >= 100000000 else (999999999 - k) + 1
+        id = all_keys[i]
         name = nomes_aleatorios[i]
         salary = round(rd.uniform(1000, 10000), 2)
         code = f'{rd.randint(1, 1000)}'
@@ -79,14 +83,14 @@ def gen_hashtable_samples(num_elements,  **kwargs):
     return data
 
 def to_gzip(obj, filename:str):
-    import gzip
+    import gzip, pickle
     with gzip.open(filename, 'wb') as f:
-        f.write(obj)
+        pickle.dump(obj, f)
 
 def from_gzip(filename:str):
-    import gzip
+    import gzip, pickle
     with gzip.open(filename, 'rb') as f:
-        return f.read()
+        return pickle.load(f)
 
 def sequential_search(arr, target):
     steps = 0
